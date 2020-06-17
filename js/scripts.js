f35853f65b280778b29ae8872217d97b31ee74e5
Erich Richter <erich.richter@gmail.com>
2020-06-17 10:51:04 -0700

    add player turns > > Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-
-
-
-
-
+// Business Logic
 Player = function(name, score, turn, id){
   this.name = name;
   this.score = score;
@@ -10,51 +6,61 @@ Player = function(name, score, turn, id){
   this.id = id;
 }
 
-
+Player.prototype.switchTurn = function(player) {
+  if (this.turn === true) {
+    return this.turn = false;
+  } else {
+    return this.turn = true;
+  }
+}
 
 Player.prototype.addScore = function(){
 
-//this.score =  
+this.score += roll;
 
-    
+    console.log()
 }
 
-// let roll = "";
-// $("#roll").click(function() {
-//   let roll = Math.floor((Math.random() * 6) + 1);
-// });
-
 // $("#hold").click(function( {
-  
+
 // })
+Player.prototype.counter = function() {
+  if (this.count%2 === 0) {
+  return true;
+  } else {
+  return false;
+  }
+}
 
-
-
-
-
-
-
-
+// User Interface Logic
 $(document).ready(function() {
-
+//player.counter()
   let playerOne = new Player("");
   let playerTwo = new Player("");
+
+  let roll = "";
 
   $("#player-info").submit(function(event) {
     event.preventDefault();
 
     playerOne.name = $("input#player-one-name").val();
-    playerOne.score = 0;
+    playerOne.score =  0;
     playerOne.turn = true;
     playerOne.id = "1";
-
+    
     playerTwo.name = $("input#player-two-name").val();
     playerTwo.score = 0;
     playerTwo.turn = false;
     playerTwo.id = "2";
 
-    console.log(playerOne);
-    console.log(playerTwo);
-
+    console.log(playerOne, playerTwo);
   });
+  
+  $("#roll").click(function() {
+    let roll = Math.floor((Math.random() * 6) + 1);
+    
+    $("#die-value").text(roll);
+  
+});
+
 });
