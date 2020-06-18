f6111b4edbcf3bbeda3405f4dae34d553511803b
Erich Richter <erich.richter@gmail.com>
2020-06-18 10:16:55 -0700

    complete functionality of game > > Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,48 +13,48 @@ Player = function(name, score, id){
 
 Game.prototype.switchTurn = function(game) {
   if (this.turn === 1) {
-    return this.turn = 2
+    return this.turn = 2 
   } else {
     return this.turn = 1
   }
 }
 
-Game.prototype.addScore = function(game) {
-  console.log(this.turn);
-  console.log(this.currentRoll);
-  console.log(this.players[0].score)
+Game.prototype.turnText = function(game) {
+  //console.log(this.players[1].name)
+  if (this.turn === 1) {
+    return this.players[0].name;
+   } else {
+   return this.players[1].name;
+   }
+}
 
+Game.prototype.addScore = function(game) {
   if (this.currentRoll === 1) {
     this.switchTurn();
+    //this.turnText();
   } else if (this.currentRoll > 1) {
     if (this.turn === this.players[0].id) {
       this.players[0].score += this.currentRoll;
+      
     } else if (this.turn === this.players[1].id) {
       this.players[1].score += this.currentRoll;
       }
   }
 }
-  // if (roll === 1) {
-  //   game.switchTurn();
-  // } else {
-  //   for (i=0; i < this.players; i++){
-  //       if (this.player.id === this.turn) {
-  //         this.player.score += roll;
-  //       }
-  //      console.log(player.score)
-  //  }
-
-//}
-//}
-
-// $("#hold").click(function( {
-
-// })
-
 
 // User Interface Logic
-$(document).ready(function() {
+Game.prototype.displayGameDetails = function(game) {
+  
+  let currentPlayer = this.turnText();
+  
+  $("#player-one").text(this.players[0].name);
+  $("#player-two").text(this.players[1].name);
+  $("#player-one-score").text(this.players[0].score);
+  $("#player-two-score").text(this.players[1].score);
+  $("#turn").text(currentPlayer);
+}
 
+$(document).ready(function() {
   let game = new Game("")
 
   let playerOne = new Player("");
@@ -65,7 +65,7 @@ $(document).ready(function() {
   game.players = [playerOne, playerTwo];
   game.turn = 1
   game.currentRoll = 0;
-
+  
   $("#player-info").submit(function(event) {
     event.preventDefault();
 
@@ -76,42 +76,22 @@ $(document).ready(function() {
     playerTwo.name = $("input#player-two-name").val();
     playerTwo.score = 0;
     playerTwo.id = 2;
+    
+    game.displayGameDetails();
+    $("#hide").show();
+    $("#show").hide();
   });
   
   $("#roll").click(function() {
-  
     roll = Math.floor((Math.random() * 6) + 1);
     $("#die-value").text(roll);
     game.currentRoll = roll;
     game.addScore();
-    console.log(playerOne, playerTwo);
-  //   if (roll === 1) {
-  //     game.switchTurn();
-  //   } else if (roll > 1) {
-  //     if (game.turn === playerOne.id) {
-  //       playerOne.score += roll;
-  //     } else if (game.turn === playerTwo.id) {
-  //       playerTwo.score += roll;
-  //       }
-  //   }
-  //   console.log(playerOne, playerTwo);
-  // });
+    game.displayGameDetails();
+  });
 
-
-    // for (let i=0; game.players.length; i++) {
-    //   if (game.players[i]) {
-    //     if (game.players[i].id == turn) {
-    //       return game.players.score += roll;
-    //     }
-    //   } else {
-    //     return false;
-    //   }
-    // }
-//    game.addScore();
-
-    // console.log(playerOne, playerTwo);
-    //playerOne, playerTwo)
-  
-//});
+  $("#hold").click(function() {
+    game.switchTurn();
+    game.displayGameDetails();
   });
 });
